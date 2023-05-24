9ca84991e73792d875eed5e511a5b27d18a24197
Vinay Kumar <vk9006379@gmail.com>
2023-05-24 23:04:21 +0530

    Added functionality to play drum using keyboard keys.

---
+++ index.js
@@ -0,0 +1,48 @@
+let buttons = document.querySelectorAll(".drum");
+
+document.addEventListener("keydown", function(event){
+    let key = event.key;
+    makeSound(key);
+});
+
+for(let i in buttons){
+    buttons[i].addEventListener("click", function(){
+        let key = this.innerHTML;
+        makeSound(key);
+    })
+}
+
+function makeSound(key){
+    switch(key){
+        case 'w':
+            let tom1 = new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        case 'a':
+            let tom2 = new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        case 's':
+            let tom3 = new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        case 'd':
+            let tom4 = new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break;
+        case 'j':
+            let snare = new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+        case 'k':
+            let crash = new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+        case 'l':
+            let kick = new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+        default:
+            console.log(key);
+    }
+}
